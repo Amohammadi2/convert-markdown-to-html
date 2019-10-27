8a637b96fbfc9ee25e31eaed41ad074a1898eeb9
Amohammadi2 <56260629+Amohammadi2@users.noreply.github.com>
2019-10-27 13:45:15 +0330

    main.py

--- markdown to HTML/main.py
+++ markdown to HTML/main.py
@@ -1,3 +1,4 @@
+import os
 import eel
 import translator
 eel.init("web")
@@ -5,4 +6,9 @@ eel.init("web")
 def handleMarkdown(markdownData:str,outpuFileName:str):
     data = translator.translate(markdownData,outpuFileName)
     return data
+@eel.expose
+def openOutputDirectory():
+    currentPath = os.getcwd()
+    targetPath = "\\web\\out"
+    os.startfile(currentPath + targetPath)
 eel.start("index.html",size=(1000,500))
